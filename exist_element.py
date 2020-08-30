f87d1319105f1902c3e490a3ffe6e36fb4af001a
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-08-30 14:06:32 +0530

    Create exist_element.py

---
+++ exist_element.py
@@ -0,0 +1,7 @@
+Prog3: Check if element exists in list in Python
+
+l1=[1,2,3,4,5]
+ele=int(input())       #enter the element to be find
+if ele in l1:
+        print("present and element is ",end=' ')
+        print(ele)
