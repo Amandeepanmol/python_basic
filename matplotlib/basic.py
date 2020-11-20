b086bc178b7cfcee922ad558335d953e28a6738e
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-11-20 22:34:33 +0530

    Create basic.py

---
+++ matplotlib/basic.py
@@ -0,0 +1,5 @@
+from matplotlib import pyplot as plt                 # importing matplotlib module  
+x = [5, 2, 9, 4, 7]                                   
+y = [10, 5, 8, 4, 2]                              
+plt.plot(x,y) 
+plt.show() 
