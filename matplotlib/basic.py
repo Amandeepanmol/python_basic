295723e9fbf81620546a5e6ece38812fe431cdf7
Amandeep Singh <47387270+Amandeepanmol@users.noreply.github.com>
2020-11-22 23:47:22 +0530

    Update basic.py

--- matplotlib/basic.py
+++ matplotlib/basic.py
@@ -1,5 +1,17 @@
+Line plot :
+-----------
+  
 from matplotlib import pyplot as plt                 # importing matplotlib module  
 x = [5, 2, 9, 4, 7]                                   
 y = [10, 5, 8, 4, 2]                              
 plt.plot(x,y) 
+plt.show() 
+
+Bar plot :
+----------
+
+from matplotlib import pyplot as plt 
+x = [5, 2, 9, 4, 7] 
+y = [10, 5, 8, 4, 2] 
+plt.bar(x,y) 
 plt.show() 
